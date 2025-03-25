82530b9c8ab40f1c2beb81fd6be9d6eb4fb39e38
shakeel <shakeel.20210426@iit.ac.lk>
2025-03-25 22:58:53 +0800

    updated

--- Assignment/Assignment/Sign_up_form.Designer.cs
+++ Assignment/Assignment/Sign_up_form.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Sign_up_form));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -48,7 +49,9 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkOrange;
             this.label1.Location = new System.Drawing.Point(241, 29);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(116, 32);
@@ -58,6 +61,8 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.DarkOrange;
             this.label2.Location = new System.Drawing.Point(62, 93);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(106, 16);
@@ -67,6 +72,8 @@
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.DarkOrange;
             this.label3.Location = new System.Drawing.Point(62, 238);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(121, 16);
@@ -76,6 +83,8 @@
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.Color.DarkOrange;
             this.label4.Location = new System.Drawing.Point(62, 190);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(162, 16);
@@ -85,6 +94,8 @@
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.ForeColor = System.Drawing.Color.DarkOrange;
             this.label5.Location = new System.Drawing.Point(62, 296);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(138, 16);
@@ -137,6 +148,8 @@
             // label7
             // 
             this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.ForeColor = System.Drawing.Color.DarkOrange;
             this.label7.Location = new System.Drawing.Point(62, 141);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(109, 16);
@@ -145,38 +158,43 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.DarkOrange;
             this.button1.Location = new System.Drawing.Point(47, 389);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(177, 53);
+            this.button1.Size = new System.Drawing.Size(190, 53);
             this.button1.TabIndex = 12;
             this.button1.Text = "Sign up";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Return
             // 
+            this.Return.BackColor = System.Drawing.Color.DarkOrange;
             this.Return.Location = new System.Drawing.Point(284, 389);
             this.Return.Name = "Return";
-            this.Return.Size = new System.Drawing.Size(162, 53);
+            this.Return.Size = new System.Drawing.Size(175, 53);
             this.Return.TabIndex = 13;
             this.Return.Text = "Log In";
-            this.Return.UseVisualStyleBackColor = true;
+            this.Return.UseVisualStyleBackColor = false;
             this.Return.Click += new System.EventHandler(this.Return_Click);
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.DarkOrange;
             this.button2.Location = new System.Drawing.Point(520, 389);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(119, 53);
+            this.button2.Size = new System.Drawing.Size(149, 53);
             this.button2.TabIndex = 14;
             this.button2.Text = "View";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Sign_up_form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1221, 785);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.Return);
@@ -195,6 +213,7 @@
             this.Controls.Add(this.label1);
             this.Name = "Sign_up_form";
             this.Text = "Sign_up_form";
+            this.Load += new System.EventHandler(this.Sign_up_form_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
