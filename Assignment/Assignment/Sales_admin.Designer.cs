0b63acc8db24dffeb4dc130b1a7c7b7f21699d87
Ryder11011 <ranjanrohan42@gmail.com>
2025-03-26 14:08:01 +0800

    updated

--- Assignment/Assignment/Sales_admin.Designer.cs
+++ Assignment/Assignment/Sales_admin.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.SalesDisplay_lbl = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -38,21 +39,35 @@
             this.panel1.BackColor = System.Drawing.Color.Red;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.SalesDisplay_lbl);
             this.panel1.Location = new System.Drawing.Point(87, 63);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(574, 517);
+            this.panel1.Size = new System.Drawing.Size(336, 146);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // SalesDisplay_lbl
+            // 
+            this.SalesDisplay_lbl.AutoSize = true;
+            this.SalesDisplay_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SalesDisplay_lbl.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.SalesDisplay_lbl.Location = new System.Drawing.Point(36, 51);
+            this.SalesDisplay_lbl.Name = "SalesDisplay_lbl";
+            this.SalesDisplay_lbl.Size = new System.Drawing.Size(168, 32);
+            this.SalesDisplay_lbl.TabIndex = 4;
+            this.SalesDisplay_lbl.Text = "10,000,000";
+            this.SalesDisplay_lbl.Click += new System.EventHandler(this.label1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(39, 20);
+            this.label1.Location = new System.Drawing.Point(74, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 29);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Sales";
             // 
             // Sales_admin
             // 
@@ -72,6 +87,7 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label SalesDisplay_lbl;
         private System.Windows.Forms.Label label1;
     }
 }
