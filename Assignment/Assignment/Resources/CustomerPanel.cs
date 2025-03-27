a2ffea228a647c590e8faf9861f5454972b5f539
Ryder11011 <ranjanrohan42@gmail.com>
2025-03-27 16:28:45 +0800

    Saving local changes before pulling

---
+++ Assignment/Assignment/Resources/CustomerPanel.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment.Resources
+{
+    public partial class CustomerMainPanel : Form
+    {
+        public CustomerMainPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void CustomerMainPanel_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+       
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Normal) 
+            
+            { 
+                WindowState = FormWindowState.Maximized;
+            }
+            else 
+            {
+                WindowState = FormWindowState.Normal;            
+            }
+         
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            WindowState= FormWindowState.Minimized;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Signing Out Already? Come Again!");
+            FormLogin formLogin = new FormLogin();
+            formLogin.Show();
+            this.Hide();
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
