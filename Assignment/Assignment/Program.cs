b93b4852ef21acc5e063912355c42dc942c4ca6c
mwilachibesa01 <mwilachibesa2006@gmail.com>
2025-04-06 18:58:43 +0800

    hi

--- Assignment/Assignment/Program.cs
+++ Assignment/Assignment/Program.cs
@@ -10,19 +10,17 @@ namespace Assignment
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-<<<<<<< HEAD
+
             Application.Run(new Main_Dashboard("mwilachibesa@gmail.com"));
-=======
-            Application.Run(new ChefPage());
->>>>>>> bf2eb99ddeb26b81d92b39f6931456da329eb1d8
+
+           
+
         }
     }
 }
