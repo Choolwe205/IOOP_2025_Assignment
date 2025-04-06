921a4bfdba2b5e507549d9e8af9dcca03a3a4a3e
Ryder11011 <ranjanrohan42@gmail.com>
2025-04-06 20:15:22 +0800

    Damn

--- Assignment/Assignment/Program.cs
+++ Assignment/Assignment/Program.cs
@@ -16,16 +16,12 @@ namespace Assignment
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-<<<<<<< HEAD
             Application.Run(new Admininstrator("SharafMo@gmail.com"));   
-=======
-
             Application.Run(new Main_Dashboard("mwilachibesa@gmail.com"));
             Application.Run(new CustomerMainPanel());
 
            
 
->>>>>>> eff118ccd83c85c75188db7f7ba5d9c7e0b1142c
         }
     }
 }
